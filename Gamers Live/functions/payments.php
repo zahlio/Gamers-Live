5393a19696c5a7345e845c7e89de7437c171cb12
Zahlio <cmhostnet@gmail.com>
2013-02-26 16:44:36 +0100

    ADDED alot // todo

---
+++ Gamers Live/functions/payments.php
@@ -0,0 +1,11 @@
+<?php
+// todo calculate what each featured partner has will get at the end of a month
+
+// should run every month on the 15th
+// should email to admin@gamers-live.net a file in the following for for mass payments
+// and add the tips amount the the database as unpaid
+
+// smith@example.com, 45, Gamers Live Tips for month.
+// smith@example.com, 45, Gamers Live Tips for month. etc etc
+
+?>
