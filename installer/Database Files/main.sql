6952b5707573e96b567ff1ecc304c4598f963394
Zahlio <cmhostnet@gmail.com>
2013-04-04 15:15:56 +0200

    ADDED the new db file with all the new features

--- installer/Database Files/main.sql
+++ installer/Database Files/main.sql
@@ -1,11 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 04, 2013 at 03:10 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `live`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_logs`
+--
 
 CREATE TABLE IF NOT EXISTS `admin_logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -15,12 +34,24 @@ CREATE TABLE IF NOT EXISTS `admin_logs` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17335 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beta`
+--
+
 CREATE TABLE IF NOT EXISTS `beta` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `b_key` varchar(255) NOT NULL,
   `active` varchar(255) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `channels`
+--
 
 CREATE TABLE IF NOT EXISTS `channels` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -31,9 +62,9 @@ CREATE TABLE IF NOT EXISTS `channels` (
   `views` varchar(255) NOT NULL DEFAULT '0',
   `online` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `info1` varchar(255) NOT NULL,
-  `info2` varchar(255) NOT NULL,
-  `info3` varchar(255) NOT NULL,
+  `info1` varchar(2555) NOT NULL,
+  `info2` varchar(2555) NOT NULL,
+  `info3` varchar(2555) NOT NULL,
   `featured` varchar(255) NOT NULL DEFAULT '0',
   `banned` varchar(255) NOT NULL DEFAULT '0',
   `viewers` varchar(255) NOT NULL DEFAULT '0',
@@ -54,6 +85,12 @@ CREATE TABLE IF NOT EXISTS `channels` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `channel_options`
+--
+
 CREATE TABLE IF NOT EXISTS `channel_options` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `option1` varchar(255) NOT NULL,
@@ -64,6 +101,12 @@ CREATE TABLE IF NOT EXISTS `channel_options` (
   `chatad2` varchar(2000) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_bans`
+--
 
 CREATE TABLE IF NOT EXISTS `chat_bans` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -77,6 +120,12 @@ CREATE TABLE IF NOT EXISTS `chat_bans` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_mods`
+--
+
 CREATE TABLE IF NOT EXISTS `chat_mods` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `channel_id` varchar(255) NOT NULL,
@@ -85,6 +134,12 @@ CREATE TABLE IF NOT EXISTS `chat_mods` (
   `moderator` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_msg`
+--
 
 CREATE TABLE IF NOT EXISTS `chat_msg` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -95,6 +150,65 @@ CREATE TABLE IF NOT EXISTS `chat_msg` (
   `type` varchar(255) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=66 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `msg` varchar(2555) NOT NULL,
+  `auther` varchar(255) NOT NULL,
+  `submitDate` varchar(255) NOT NULL,
+  `endDate` varchar(255) NOT NULL,
+  `views` varchar(255) NOT NULL,
+  `channel` varchar(255) NOT NULL,
+  `featured` varchar(255) NOT NULL,
+  `startDate` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `game` varchar(255) NOT NULL,
+  `featuredShow` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_comments`
+--
+
+CREATE TABLE IF NOT EXISTS `event_comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `auther` varchar(255) NOT NULL,
+  `msg` varchar(2555) NOT NULL,
+  `dateSend` varchar(255) NOT NULL,
+  `event_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_wanna`
+--
+
+CREATE TABLE IF NOT EXISTS `event_wanna` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` varchar(255) NOT NULL,
+  `viewer` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `startDate` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `frontpage`
+--
 
 CREATE TABLE IF NOT EXISTS `frontpage` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -109,6 +223,12 @@ CREATE TABLE IF NOT EXISTS `frontpage` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games`
+--
+
 CREATE TABLE IF NOT EXISTS `games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `game` varchar(255) NOT NULL,
@@ -118,6 +238,28 @@ CREATE TABLE IF NOT EXISTS `games` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kbase`
+--
+
+CREATE TABLE IF NOT EXISTS `kbase` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `auther` varchar(255) NOT NULL,
+  `msg` varchar(2555) NOT NULL,
+  `dateSend` varchar(255) NOT NULL,
+  `cat` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_log`
+--
+
 CREATE TABLE IF NOT EXISTS `login_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
@@ -125,7 +267,13 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `time` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=98 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=115 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `partner_payments`
+--
 
 CREATE TABLE IF NOT EXISTS `partner_payments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -139,6 +287,12 @@ CREATE TABLE IF NOT EXISTS `partner_payments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `partner_payments_to_do`
+--
+
 CREATE TABLE IF NOT EXISTS `partner_payments_to_do` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `partner_id` varchar(255) NOT NULL,
@@ -149,6 +303,12 @@ CREATE TABLE IF NOT EXISTS `partner_payments_to_do` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribtions`
+--
+
 CREATE TABLE IF NOT EXISTS `subscribtions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fan_channel_id` varchar(255) NOT NULL,
@@ -156,6 +316,37 @@ CREATE TABLE IF NOT EXISTS `subscribtions` (
   `date` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE IF NOT EXISTS `tickets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `msg` varchar(25555) NOT NULL,
+  `isTicket` varchar(255) NOT NULL,
+  `isReply` varchar(255) NOT NULL,
+  `replySender` varchar(255) NOT NULL,
+  `originTicketID` varchar(255) NOT NULL,
+  `isStaff` varchar(255) NOT NULL,
+  `dateSend` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `partner` varchar(255) NOT NULL,
+  `banned` varchar(255) NOT NULL,
+  `userName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tips`
+--
 
 CREATE TABLE IF NOT EXISTS `tips` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -166,6 +357,12 @@ CREATE TABLE IF NOT EXISTS `tips` (
   `2co_id` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tips_payza`
+--
 
 CREATE TABLE IF NOT EXISTS `tips_payza` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -187,7 +384,13 @@ CREATE TABLE IF NOT EXISTS `tips_payza` (
   `p_year` varchar(255) NOT NULL,
   `pending` varchar(255) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -195,8 +398,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `avatar` varchar(255) NOT NULL,
-  `short_bio` varchar(255) NOT NULL,
-  `long_bio` varchar(255) NOT NULL,
+  `short_bio` varchar(2555) NOT NULL,
+  `long_bio` varchar(2555) NOT NULL,
   `timezone` varchar(255) NOT NULL,
   `channel_id` varchar(255) NOT NULL,
   `banned` varchar(255) NOT NULL,
